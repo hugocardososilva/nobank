459acab67d566673f831fcb29d264442e227b397
Hugo Silva <hugocardososilva@gmail.com.br>
2018-09-13 18:23:33 -0300

    Alterando versão do ruby

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Banco-Integra_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "banco-integra_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
